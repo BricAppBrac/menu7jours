651cc8bf004606b02303a48504a3815d3aa3c9a8
BricAppBrac <sandrinesteiger3000@gmail.com>
2023-07-25 10:14:15 +0200

    ajout netlify-cli

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,3 +12,30 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+// const connectDB = async (_client) => {
+//   // create a new connection if one doesn't already exist
+//   if (!_client) {
+//     mongoose.set("strictQuery", false);
+//     _client = await mongoose.connect(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log("BDD MongoDB connectée");
+//   }
+
+//   return _client;
+// };
+
+// const close = async (_client) => {
+//   // destroy the connection if it exists
+//   if (_client) {
+//     await _client.disconnect();
+//     _client = null;
+//   }
+// };
+
+// module.exports = {
+//   connectDB,
+//   close,
+// };
